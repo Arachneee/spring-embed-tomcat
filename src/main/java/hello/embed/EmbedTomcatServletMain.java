87702a493e521ce9878d776b088b7a1c1c2f36af
Arachne <jhg2819@naver.com>
2023-08-20 18:41:26 +0900

    spring embedTomcat

---
+++ src/main/java/hello/embed/EmbedTomcatServletMain.java
@@ -0,0 +1,35 @@
+package hello.embed;
+
+import java.io.File;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.startup.Tomcat;
+
+import hello.servlet.HelloServlet;
+
+public class EmbedTomcatServletMain {
+	public static void main(String[] args) throws LifecycleException {
+		System.out.println("EmbedTomcatServletMain.main");
+
+		Tomcat tomcat = new Tomcat();
+		Connector connector = new Connector();
+		connector.setPort(8080);
+		tomcat.setConnector(connector);
+
+		Context context = tomcat.addContext("", "/");
+		//== 코드 추가 시작==
+		File docBaseFile = new File(context.getDocBase());
+		if (!docBaseFile.isAbsolute()) {
+			docBaseFile = new File(((org.apache.catalina.Host)
+				context.getParent()).getAppBaseFile(), docBaseFile.getPath());
+		}
+		docBaseFile.mkdirs();
+		//== 코드 추가 종료==
+		tomcat.addServlet("", "helloServlet", new HelloServlet());
+		context.addServletMappingDecoded("/hello-servlet", "helloServlet");
+		tomcat.start();
+	}
+
+}
